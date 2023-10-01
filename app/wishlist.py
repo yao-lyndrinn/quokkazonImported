e8ce57ecbbf4c9b2a7788e6da389e9e614ec5ca5
Amy Weng <acw77@duke.edu>
2023-10-01 19:26:45 -0400

    yay again!

--- app/wishlist.py
+++ app/wishlist.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from flask import render_template
 from flask_login import current_user
+from flask import redirect, url_for
 import datetime
 
 from .models.wishlist import WishListItem
@@ -11,7 +12,7 @@ bp = Blueprint('wishlist', __name__)
 
 @bp.route('/wishlist')
 def wishlist():
-    # find the items the current user has added to their wishlist:
+    # find the items the current user has added to their wishlist
     if current_user.is_authenticated:
         items = WishListItem.get_all_by_uid_since(
                         current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
@@ -19,3 +20,9 @@ def wishlist():
         return jsonify({}), 404
     # render the page by adding information to the index.html file
     return jsonify([item.__dict__ for item in items])
+
+@bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
+def wishlist_add(product_id):
+    current_dateTime = datetime.datetime.now()
+    WishListItem.add_item(current_user.id, product_id, current_dateTime)
+    return redirect(url_for('wishlist.wishlist'))

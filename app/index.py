4ff73b11bbcb78506e0845bff5d1d8c84fefc96c
Amy Weng <acw77@duke.edu>
2023-10-01 21:42:11 -0400

    finallygit add .

--- app/index.py
+++ app/index.py
@@ -12,14 +12,20 @@ bp = Blueprint('index', __name__)
 @bp.route('/')
 def index():
     # get all available products for sale:
-    products = Product.get_all(True)
+    products = Product.get_all()
+    
     # find the products current user has bought:
     if current_user.is_authenticated:
         purchases = Purchase.get_all_by_uid_since(
             current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
+        print("THIS IS THE FIRST", purchases)
+        print("This is current_user.id", current_user.id)
+        print("This is datetime", datetime.datetime(1980, 9, 14, 0, 0, 0))
     else:
         purchases = None
+        print("ITS NON BITCH")
     # render the page by adding information to the index.html file
     return render_template('index.html',
                            avail_products=products,
                            purchase_history=purchases)
+    
--- app/models/purchase.py
+++ app/models/purchase.py
@@ -1,32 +1,34 @@
 from flask import current_app as app
 
-
 class Purchase:
-    def __init__(self, id, uid, pid, time_purchased):
-        self.id = id
+    def __init__(self, uid, sid, pid, order_id, time_purchased, quantity, date_fulfilled):
         self.uid = uid
+        self.sid = sid
         self.pid = pid
+        self.order_id = order_id
         self.time_purchased = time_purchased
+        self.quantity = quantity
+        self.date_fulfilled = date_fulfilled
 
     @staticmethod
-    def get(id):
+    def get(uid,pid,sid,order_id):
         rows = app.db.execute('''
-SELECT id, uid, pid, time_purchased
-FROM Purchases
-WHERE id = :id
-''',
-                              id=id)
+        SELECT uid, sid, pid, order_id, time_purchased, quantity, date_fulfilled
+        FROM Purchases
+        WHERE uid = :uid AND pid = :pid AND sid =:sid AND order_id = :order_id
+        ''',
+        uid=uid, pid=pid, sid =sid, order_id = order_id)
         return Purchase(*(rows[0])) if rows else None
 
     @staticmethod
     def get_all_by_uid_since(uid, since):
         rows = app.db.execute('''
-SELECT id, uid, pid, time_purchased
-FROM Purchases
-WHERE uid = :uid
-AND time_purchased >= :since
-ORDER BY time_purchased DESC
-''',
+        SELECT *
+        FROM Purchases
+        WHERE uid = :uid
+        AND time_purchased >= :since
+        ORDER BY time_purchased DESC
+        ''',
                               uid=uid,
                               since=since)
         return [Purchase(*row) for row in rows]

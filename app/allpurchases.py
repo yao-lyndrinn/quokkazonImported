9a888056e5e1cae495c84e94d3b20f7f9328da21
Phillip Ding <pcd13@duke.edu>
2023-10-04 12:34:47 -0400

    milestone button

--- app/allpurchases.py
+++ app/allpurchases.py
@@ -11,7 +11,10 @@ def allpurchases():
     if request.method == 'POST':
         uid = request.form.get('uid')
         # Fetch all purchases for the given uid since a very old date
-        purchases = Purchase.get_all_by_uid_since(uid, datetime.datetime(1980, 1, 1, 0, 0, 0))
-        return render_template('allpurchases.html', purchases=purchases)
+        if uid != '' and uid.isnumeric():
+            purchases = Purchase.get_all_by_uid_since(uid, datetime.datetime(1980, 1, 1, 0, 0, 0))
+            return render_template('allpurchases.html', purchases=purchases)
+        else:
+            return jsonify({}), 404
     return render_template('allpurchases.html', purchases=[])
 

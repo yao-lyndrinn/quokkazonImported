2d70f53bbe814c3792aded16f0c3c0e2b8c1deab
Jonathan Shi <jjs118@duke.edu>
2023-10-03 22:08:06 -0400

    Showing product names, sortable table

--- app/inventory.py
+++ app/inventory.py
@@ -1,16 +1,18 @@
 from flask import render_template
 from flask_login import current_user
+from flask import redirect, url_for
 import datetime
 
 from .models.inventory import Inventory
+from .models.seller import Seller
+from .models.product import Product
 
 from flask import Blueprint
 bp = Blueprint('inventory', __name__)
 
 
 @bp.route('/inventory')
-def index():
-    
+def inventory():
     # find the products current user has bought:
     if current_user.is_authenticated:
         inventory = Inventory.get_all_by_sid(
@@ -19,5 +21,6 @@ def index():
         inventory = None
     # render the page by adding information to the index.html file
     return render_template('inventory.html',
-                           avail_inventory=inventory)
-    
+                           avail_inventory=inventory,
+                           is_seller=Seller.is_seller(current_user),
+                           product_class=Product)

ca2eb6dd2ddfab818d22f4093671b9fb021071e4
elise_zhang <ez37@duke.edu>
2023-10-04 00:50:14 -0400

    Finished top k products form

--- app/products.py
+++ app/products.py
@@ -1,11 +1,11 @@
-from flask import jsonify
 from flask import render_template
-from flask_login import current_user
-from flask import redirect, url_for
-import datetime
+from flask import redirect, request, url_for
 from humanize import naturaltime
+from collections import defaultdict
 
 from .models.product import Product
+from .models.inventory import Inventory
+from .models.stock import Stock
 
 from flask import Blueprint
 bp = Blueprint('products', __name__)
@@ -15,8 +15,25 @@ def productDetail(product_id):
     product = Product.get(product_id)
     return render_template('productDetail.html',
                            product=product)
-@bp.route('/products')
+        
+@bp.route('/products', methods=['GET','POST'])
 def products():
     items = Product.get_all()
+    inventory = Inventory.get_all()
+    product_prices = defaultdict(list)
+    for item in inventory:
+        product_prices[item.pid].append(item.price)
+    if request.method == 'POST':
+        products = Stock.get_all_in_stock()
+        k_prod = []
+        k = int(request.form['k'])
+        for i in range(k):
+            k_prod.append(products[i])
+        print("KPROODDDDD", k_prod)
+        print("PRODUCTSSSSS", products)
+        return render_template('topProducts.html',
+                    k_prod = k_prod, k = k)
+    
     return render_template('products.html',
-                      items=items)
+                      items=items, inventory=inventory, product_prices = product_prices)
+    

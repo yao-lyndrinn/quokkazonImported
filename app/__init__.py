d40c9a521bf8c763ae0b8590557dcb46d77d0781
Phillip Ding <pcd13@duke.edu>
2023-11-08 19:04:25 -0500

    profile page

--- app/__init__.py
+++ app/__init__.py
@@ -39,4 +39,7 @@ def create_app():
     from .cart import bp as cart_bp
     app.register_blueprint(cart_bp)
 
+    from .profile import bp as profile_bp
+    app.register_blueprint(profile_bp)
+    
     return app

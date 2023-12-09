a82253ce1df9ca3fb9ffc0dac8e02527fde2c4d2
elise_zhang <ez37@duke.edu>
2023-12-08 19:20:39 -0500

    Fixed editProduct

---
+++ db/data/cleaning_scripts/add_creators.py
@@ -0,0 +1,15 @@
+import csv
+import random
+
+input_file = '/home/ubuntu/quokkazon/db/data/Products2.csv'
+output_file = '/home/ubuntu/quokkazon/db/data/Products.csv'
+
+sellers = ["2", "5", "6", "8", "9", "10", "12", "13", "14", "22", "31", "36", "39", "42", "50", "51", "52", "65", "81", "82", "96", "98"]
+with open(input_file, 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    rows = [row + [random.choice(sellers)] for row in reader]
+
+
+with open(output_file, 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerows(rows)

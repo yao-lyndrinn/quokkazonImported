2c9cff42a55f005caf80d837971d54eb87cb593e
Jonathan Shi <jjs118@duke.edu>
2023-12-08 20:46:50 -0500

    idk what i was doing

--- app/inventory.py
+++ app/inventory.py
@@ -13,31 +13,28 @@ from .models.product import Product
 from flask import Blueprint
 bp = Blueprint('inventory', __name__)
 
-
+# load inventory table page, showing only the inventory entries a seller has access to
 @bp.route('/inventory')
 def inventory():
     # find the products current user has bought:
     if current_user.is_authenticated:
         inventory = Inventory.get_all_by_sid(
             current_user.id)
-        products = Product.get_all_by_sid(current_user.id)
-        print("THESE ARE PRODUCTSSSS", products)
     else:
         inventory = None
     # render the page by adding information to the index.html file
     return render_template('inventory.html',
                            avail_inventory=inventory,
                            is_seller=Seller.is_seller(current_user),
-                           product_class=Product,
-                           products=products)
+                           product_class=Product)
 
-class InventoryEdit(FlaskForm):
+class InventoryEdit(FlaskForm): # Form for editing Inventory item values in a seller's inventory
     quantity = IntegerField('Quantity', validators=[NumberRange(min=0)])
     num_for_sale = IntegerField('For sale (≤ Quantity)', validators=[NumberRange(min=0,)])
     price = FloatField('Price', validators=[NumberRange(min=0,)])
     save = SubmitField('Save')
 
-
+# Edit the price, quantity, and amount for sale of a product for a particular seller in their inventory
 @bp.route('/inventory/edit/<int:product_id>/<int:oq>-<int:on>:<float:op>', methods=['GET', 'POST'])
 def edit(product_id, oq, on, op):
     form = InventoryEdit()
@@ -51,6 +48,7 @@ def edit(product_id, oq, on, op):
             return redirect(url_for('inventory.inventory'))
     return render_template('editInventory.html', title="Edit Item", form=form, old_quantity=oq, old_num_for_sale=on, old_price=op, product_name=Product.get_name(product_id), product_id=product_id)
 
+# Add an existing product to a seller's own inventory if it is not already present
 @bp.route('/inventory/add/<int:product_id>', methods=['GET','POST'])
 def add(product_id):
     default_quantity = 0
@@ -61,6 +59,7 @@ def add(product_id):
     flash(Product.get_name(product_id) + ' already present in inventory!')
     return redirect(url_for('inventory.inventory'))
 
+# Remove a product from only a seller's own inventory
 @bp.route('/inventory/delete/<int:product_id>', methods=['GET', 'POST'])
 def delete(product_id):
     flash(Product.get_name(product_id) + ' deleted from inventory!')

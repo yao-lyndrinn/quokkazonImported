a95107b3af7ab158baa1c08cf98fd42bd2f143c5
elise_zhang <ez37@duke.edu>
2023-12-08 20:06:39 -0500

    some comments

--- app/categories.py
+++ app/categories.py
@@ -18,12 +18,17 @@ from .models.seller import Seller
 from flask import Blueprint
 bp = Blueprint('categories', __name__)
 
+#human readable time
 def humanize_time(dt):
     return naturaltime(datetime.datetime.now() - dt)
 
+# number of items on each page is 24
 ROWS = 24
+
+# function used to display the products filtered by category using the same products template
 @bp.route('/<category_id>', methods=['GET','POST'])
 def category_products(category_id):
+    # defining the page and items on page
     page = request.args.get("page", 1, type=int)
     start = (page-1) * ROWS
     end = start + ROWS
@@ -34,21 +39,23 @@ def category_products(category_id):
     product_prices = defaultdict(list)
     summary = defaultdict(list)
     
+    # filtering and sorting based on the page request
     if request.method == 'GET':
         filter_by = request.args.get('filter_by') if request.args.get('filter_by') is not None else 'all'
         sort_by = request.args.get('sort_by') if request.args.get('sort_by') is not None else 'a-z'
-        if sort_by == "top_reviews":
-            items = ProductRating.all_ratings_cid(category_id)
+        if sort_by == "top_reviews": 
+            items = ProductRating.all_ratings_cid(category_id) #reviews are available for available products
         elif sort_by == "low_price":
-            items = Stock.get_stock_by_cat_asc(category_id)
+            items = Stock.get_stock_by_cat_asc(category_id) #price is available for in-stock
         elif sort_by == "high_price":
-            items = Stock.get_stock_by_cat_desc(category_id)
-        else:
+            items = Stock.get_stock_by_cat_desc(category_id) #price is available for in-stock
+        else: # sorting by alphabet is ok both in and not in stock
             if filter_by == "available":
-                items = apply_sort(Stock.get_stock_by_cat(category_id), sort_by)
+                items = apply_sort(Stock.get_stock_by_cat(category_id), sort_by) 
             else:
                 items = apply_sort(cat_items, sort_by)
     
+    #retrieving prices and ratings for each product from inventory and feedback tables
     for item in inventory:
         product_prices[item.pid].append(item.price)
         summary[item.pid] = ProductFeedback.summary_ratings(item.pid)
@@ -56,6 +63,7 @@ def category_products(category_id):
     paginated = items[start:end]
     total_pages = len(items)//24 + 1
     
+    #retrieving categories from category table
     categories = Category.get_all()
     
     return render_template('products.html', items=paginated,
